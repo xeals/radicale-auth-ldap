b870918512e68544fb1452c613bf1eaa04010c60
xeals <dev@xeal.me>
2022-08-27 18:55:48 +1000

    Fix failing Radicale 3 tests

--- test/integration_tests.py
+++ test/integration_tests.py
@@ -18,9 +18,9 @@ class Authentication(unittest.TestCase):
         cls.logger = logging.getLogger(__name__)
 
     def test_authentication_works(self):
-        auth = radicale_auth_ldap.Auth(self.__class__.configuration, self.__class__.logger)
+        auth = radicale_auth_ldap.Auth(self.__class__.configuration)
         self.assertTrue(auth.is_authenticated(VALID_USER, VALID_PASS))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
